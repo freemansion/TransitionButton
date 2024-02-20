ae964c9e282a528acec270e68cddf3921f5f4778
Stanislau Baranouski <stan.baranouski@thechefz.co>
2024-02-20 14:15:03 +0300

    fixed SPM support

--- Package.swift
+++ Package.swift
@@ -1,12 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "TransitionButton",
     platforms: [
-        .iOS(.v8)
+        .iOS(.v13)
     ],
     products: [
         .library(
@@ -15,7 +14,6 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "TransitionButton",
-            dependencies: []),
+            name: "TransitionButton", path: "Sources")
     ]
 )
